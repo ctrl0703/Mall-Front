f334448f12de087d5de3eeef64912735cd3d43ee
cahlee <cahlee@kr.ibm.com>
2020-02-13 15:56:43 +0900

    first commit

---
+++ src/main/java/io/cahlee/MallFrontApplication.java
@@ -0,0 +1,13 @@
+package io.cahlee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MallFrontApplication.class, args);
+	}
+
+}

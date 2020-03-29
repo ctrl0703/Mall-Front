930fe00e1d60b2d3d54d25c8741a807bce435645
ctrl0703 <ctrl0703@naver.com>
2020-03-29 11:38:26 +0900

    평균 평점 계산

--- src/main/java/com/ibm/kr/domain/ReviewInfoDTO.java
+++ src/main/java/com/ibm/kr/domain/ReviewInfoDTO.java
@@ -8,4 +8,8 @@ public class ReviewInfoDTO {
 	private String avgScore = "0";
 	private String totCnt = "0";
 	
+	public String getAvgScorePercent() {
+		return 100 / 5 * Double.parseDouble(avgScore) + "%";
+	}
+	
 }
